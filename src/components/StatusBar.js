c5dcaf88af7d16ad99cd0870c38709efe11cb5d8
Ryan Boris <ryan.boris@gmail.com>
2020-03-02 23:32:22 -0500

    :construction: update statusbar

--- src/components/StatusBar.js
+++ src/components/StatusBar.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { wins } from '../wins'
+import GameContext from './GameContext'
 
-const StatusBar = () => {
-  return <div>StatusBar</div>
+const GameStatus = props => {
+  const { state } = useContext(GameContext)
+
+  if (
+    (!wins.includes(props.playerOneIds) ||
+      !wins.includes(props.playerTwoIds)) &&
+    state.moveCount === 16
+  ) {
+    return <div>GAME OVER DRAW!</div>
+  } else {
+    return <div>{state.currentPlayer}</div>
+  }
 }
 
-export default StatusBar
+export default GameStatus
