058c366fc9a17869a99a4fd2116bc8f2a62be692
Ryan Boris <ryan.boris@gmail.com>
2020-03-02 23:31:19 -0500

    :construction: update hole

--- src/components/Hole.js
+++ src/components/Hole.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import GameContext from './GameContext'
 
-const Hole = () => {
-  return <div>Hole</div>
+import './styles/Hole.css'
+
+const Hole = props => {
+  const { state, dispatch } = useContext(GameContext)
+
+  function handleMove() {
+    const notOccupied = []
+
+    const row = state.holes.filter(hole => {
+      return (hole.id - props.hole.id) % 4 === 0
+    })
+    for (const obj of row) {
+      if (!obj.isOccupied) {
+        notOccupied.push(obj)
+      }
+    }
+
+    if (notOccupied.length === 0) {
+      return
+    }
+
+    return dispatch({
+      type: state.currentPlayer || 'player1',
+      id: notOccupied[notOccupied.length - 1].id
+    })
+  }
+
+  return (
+    <button className={`Hole ${props.hole.tokenColor}`} onClick={handleMove}>
+      {props.hole.id}
+    </button>
+  )
 }
 
 export default Hole
