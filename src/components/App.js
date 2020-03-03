3286609f642af02a88906e72aca1f3e22f1d3301
Ryan Boris <ryan.boris@gmail.com>
2020-03-02 23:26:20 -0500

    :construction: update app

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useReducer, useContext } from 'react'
+import GameReducer from '../store/GameReducer'
+import GameContext from '../store/GameContext'
+import Board from './Board'
 
 const App = () => {
-  return <div>APP!</div>
+  const initialState = useContext(GameContext)
+  const [state, dispatch] = useReducer(GameReducer, initialState)
+
+  return (
+    <GameContext.Provider value={{ state, dispatch }}>
+      <Board />
+    </GameContext.Provider>
+  )
 }
 
 export default App
---
+++ src/store/GameReducer.js
@@ -0,0 +1,3 @@
+export default function GameReducer(state, action) {
+  return
+}
