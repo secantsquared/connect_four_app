181f5917fe279d2702f77b961e9800a4f9cfae36
Ryan Boris <ryan.boris@gmail.com>
2020-03-02 23:22:24 -0500

    :construction: Add component boilerplates

---
+++ src/components/Hole.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Hole = () => {
+  return <div>Hole</div>
+}
+
+export default Hole
---
+++ src/components/StatusBar.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const StatusBar = () => {
+  return <div>StatusBar</div>
+}
+
+export default StatusBar
